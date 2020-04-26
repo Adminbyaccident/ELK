89e48336963966cc5e4f6b48195a5459c75cbe7c
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2020-04-26 14:47:57 +0200

    Create elk-stack_centos8.sh

---
+++ elk-stack_centos8.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/bash
+
+# This is an install script for an ELK stack on CentOS 8.
+
+# Instructions on how to use this script 
+
+# chmod +x SCRIPTNAME.sh
+
+# sudo ./SCRIPTNAME.sh
+
+# Let's update CentOS local repositories on this box.
+yum update -y
+
+# Let's upgrade the already installed packages on this box.
+yum upgrade -y
+
+# Allow HTTP through the firewall
+firewall-cmd --permanent --zone=public --add-port=5044/tcp
+firewall-cmd --permanent --zone=public --add-port=5601/tcp
+firewall-cmd --reload
+
+# Enable EPEL repository
+yum --enablerepo=extras install epel-release
+
+# Install Java
+yum install -y java-latest-openjdk.x86_64
+
+# Import Elastic's repository key
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+
+# Add a configuration file for the elasticsearch repository
+echo "
+[elasticsearch]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=0
+autorefresh=1
+type=rpm-md
+" >> /etc/yum.repos.d/elasticsearch.repo
+
+# Enable the just configured repo
+yum install -y --enablerepo=elasticsearch elasticsearch
+
+# Configure elasticsearch itself
+sed -i 's/#network.host: 192.168.0.1/network.host: localhost/' /etc/elasticsearch/elasticsearch.yml
+
+sed -i 's/#http.port:/http.port:/' /etc/elasticsearch/elasticsearch.yml
+
+# Enable elasticsearch as a service controlled by systemd
+systemctl enable elasticsearch.service
+
+# Start elasticsearch
+systemctl start elasticsearch.service
+
+# Add a configuration file for the kibana repository
+echo "
+[kibana-7.x]
+name=Kibana repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+" >> /etc/yum.repos.d/kibana.repo
+
+# Install kibana
+yum install -y kibana
+
+# Configure kibana itself
+sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/' /etc/kibana/kibana.yml
+
+sed -i 's/#server.port: 5601/server.port: 5601/' /etc/kibana/kibana.yml
+
+# Enable kibana as a service controlled by systemd
+systemctl enable kibana.service
+
+# Start up kibana
+systemctl start kibana.service
+
+# Add a configuration file for the logstash repository
+echo "
+[logstash-7.x]
+name=Elastic repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+" >> /etc/yum.repos.d/logstash.repo
+
+# Install logstash
+yum install -y logstash
+
+# Configure logstash itself
+sed -i 's/#http.host: "127.0.0.1"/http.host:"127.0.0.1"/'  /etc/logstash/logstash.yml
+
+# Enable logstash as a service controlled by systemd
+systemctl enable logstash.service
+
+# Start up logstash
+systemctl start logstash.service
+
+# Final message 1
+echo "ELK stack installed"
+
+# Final message 2
+echo "Visit this URL 'http://yourserverip:5601' in order to use the ELK stack"
